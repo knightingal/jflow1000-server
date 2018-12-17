2d646a52df87cf197ed4c0f05bd0f5a0d77dc219
knightingal <yujianming1985@hotmail.com>
2018-12-17 23:19:35 +0800

    add FileUtil

---
+++ src/main/java/org/nanking/knightingal/util/FileUtil.java
@@ -0,0 +1,18 @@
+package org.nanking.knightingal.util;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FileUtil {
+
+    public String getFileNameByUrl(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            return new File(url.getFile()).getName();
+        } catch (MalformedURLException e) {
+            return null;
+        }
+
+    }
+}
