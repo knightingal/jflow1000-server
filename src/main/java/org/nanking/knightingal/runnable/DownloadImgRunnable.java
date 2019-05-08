ccbfe9999315c6e4b94c900242536443d61742b8
knightingal <yujianming1985@hotmail.com>
2019-05-08 09:20:46 +0800

    update

--- src/main/java/org/nanking/knightingal/runnable/DownloadImgRunnable.java
+++ src/main/java/org/nanking/knightingal/runnable/DownloadImgRunnable.java
@@ -3,8 +3,9 @@ package org.nanking.knightingal.runnable;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.nanking.knightingal.bean.Flow1000Img;
-import org.nanking.knightingal.bean.Urls1000Body;
 import org.nanking.knightingal.dao.Local1000Dao;
 import org.nanking.knightingal.util.ApplicationContextProvider;
 import org.nanking.knightingal.util.EncryptUtil;
@@ -19,10 +20,13 @@ import java.util.concurrent.CountDownLatch;
 
 public class DownloadImgRunnable implements Runnable {
 
+    private static final Log log = LogFactory.getLog(DownloadImgRunnable.class);
 
-    private EncryptUtil encryptUtil = (EncryptUtil) ApplicationContextProvider.getBean("encryptUtil");
+    private final static EncryptUtil encryptUtil = (EncryptUtil) ApplicationContextProvider.getBean("encryptUtil");
 
-    private Local1000Dao local1000Dao = (Local1000Dao) ApplicationContextProvider.getBean("local1000Dao");
+    private final static Local1000Dao local1000Dao = (Local1000Dao) ApplicationContextProvider.getBean("local1000Dao");
+
+    private final static OkHttpClient client = (OkHttpClient) ApplicationContextProvider.getBean("client");
 
     private final String dirName;
 
@@ -30,7 +34,6 @@ public class DownloadImgRunnable implements Runnable {
 
     private final CountDownLatch countDownLatch;
 
-    OkHttpClient client = new OkHttpClient();
 
     public DownloadImgRunnable(Flow1000Img flow1000Img, String dirName, CountDownLatch countDownLatch) {
         this.dirName = dirName;
@@ -51,7 +54,7 @@ public class DownloadImgRunnable implements Runnable {
     public void run() {
         String fileName = flow1000Img.getName();
 
-        System.out.println("start to download " + flow1000Img.getSrc() + " to dirName " + dirName);
+        log.info("start to download " + flow1000Img.getSrc() + " to dirName " + dirName);
         Request request = new Request.Builder().url(flow1000Img.getSrc()).
                 addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36").
                 addHeader("Connection", "keep-alive").
@@ -71,7 +74,7 @@ public class DownloadImgRunnable implements Runnable {
             File file = new File(absPath + fileName);
             boolean createRet = file.createNewFile();
             if (!createRet) {
-                System.out.println("cannot create " + absPath);
+                log.error("cannot create " + absPath);
                 return;
             }
             FileOutputStream fileOutputStream = new FileOutputStream(file);
@@ -81,7 +84,7 @@ public class DownloadImgRunnable implements Runnable {
             BufferedImage sourceImg = ImageIO.read(new FileInputStream(file));
             int width = sourceImg.getWidth();
             int height = sourceImg.getHeight();
-            System.out.println("file name:" + fileName + " width:" + width + " height:" + height);
+            log.info("file name:" + fileName + " width:" + width + " height:" + height);
 
             byte[] encryptedBytes = encryptUtil.encrypt(respBytes);
             absPath = "/home/knightingal/download/linux1000/encrypted/" + dirName + "/";
@@ -90,7 +93,7 @@ public class DownloadImgRunnable implements Runnable {
             file = new File(absPath + fileName + ".bin");
             createRet = file.createNewFile();
             if (!createRet) {
-                System.out.println("cannot create " + absPath);
+                log.error("cannot create " + absPath);
                 return;
             }
             fileOutputStream = new FileOutputStream(file);
@@ -103,8 +106,7 @@ public class DownloadImgRunnable implements Runnable {
             e.printStackTrace();
         }
 
-
-        System.out.println(flow1000Img.getSrc() + " download end");
+        log.info(flow1000Img.getSrc() + " download end");
         countDownLatch.countDown();
     }
 }
