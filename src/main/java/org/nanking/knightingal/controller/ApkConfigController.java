dfb0c8defa975230b80fc4b37d39034d0239251e
knightingal <yujianming1985@hotmail.com>
2023-10-25 22:55:32 +0800

    fix apk upload api

--- src/main/java/org/nanking/knightingal/controller/ApkConfigController.java
+++ src/main/java/org/nanking/knightingal/controller/ApkConfigController.java
@@ -1,18 +1,32 @@
 package org.nanking.knightingal.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
 
 @RequestMapping("/apkConfig")
 @RestController
 public class ApkConfigController {
 
+    @Value("${apk.filepath.base}")
+    private String apkFilePathBase;
     @PostMapping("/upload")
-    public ResponseEntity<Object> uploadPackage(File apk) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Object> uploadPackage(@RequestParam("file") MultipartFile file) {
+        String fileName = file.getOriginalFilename();
+        String filePath = apkFilePathBase + fileName;
+        File dest = new File(filePath);
+        try {
+            file.transferTo(dest);
+            return ResponseEntity.ok().build();
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
