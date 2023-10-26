87dbd6abfea0557425fda4e87a94e19ed08500ba
knightingal <yujianming1985@hotmail.com>
2023-10-26 23:38:59 +0800

    fix apk upload api

--- src/main/java/org/nanking/knightingal/controller/ApkConfigController.java
+++ src/main/java/org/nanking/knightingal/controller/ApkConfigController.java
@@ -1,5 +1,10 @@
 package org.nanking.knightingal.controller;
 
+import com.android.ide.common.process.ProcessException;
+import com.android.tools.apk.analyzer.AaptInvoker;
+import com.android.tools.apk.analyzer.AndroidApplicationInfo;
+import com.android.tools.apk.analyzer.ApkAnalyzerCli;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,10 +15,13 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 @RequestMapping("/apkConfig")
 @RestController
+@Slf4j
 public class ApkConfigController {
+
 
     @Value("${apk.filepath.base}")
     private String apkFilePathBase;
@@ -24,9 +32,20 @@ public class ApkConfigController {
         File dest = new File(filePath);
         try {
             file.transferTo(dest);
+
+            AaptInvoker aaptInvoker = ApkAnalyzerCli.getAaptInvokerFromSdk("/home/knightingal/Android/Sdk");
+            List<String> dumpBadgings = aaptInvoker.dumpBadging(dest);
+
+            AndroidApplicationInfo apkInfo = AndroidApplicationInfo.parseBadging(dumpBadgings);
+            log.debug("versionName:{}" , apkInfo.versionName);
+            log.debug("versionCode:{}" , apkInfo.versionCode);
+            log.debug("packageId:{}" , apkInfo.packageId);
+
             return ResponseEntity.ok().build();
         } catch (IOException e) {
             return ResponseEntity.internalServerError().build();
+        } catch (ProcessException e) {
+            throw new RuntimeException(e);
         }
     }
 }
