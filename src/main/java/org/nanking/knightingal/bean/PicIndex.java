f2f02f0ad599e6ded1e6ff28f035d398ed12fd3a
御铭 <yujianming1985@126.com>
2023-11-12 21:12:17 +0800

    remove unused setter in PicIndex

--- src/main/java/org/nanking/knightingal/bean/PicIndex.java
+++ src/main/java/org/nanking/knightingal/bean/PicIndex.java
@@ -25,39 +25,6 @@ public class PicIndex {
     private String album;
 
     private ClientStatus clientStatus;
-
-    public void setAlbum(String album) {
-        this.album = album;
-    }
-
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setMtime(String mtime) {
-        this.mtime = mtime;
-    }
-
-    public void setCover(String cover) {
-        this.cover = cover;
-    }
-
-    public void setCoverWidth(int coverWidth) {
-        this.coverWidth = coverWidth;
-    }
-
-    public void setCoverHeight(int coverHeight) {
-        this.coverHeight = coverHeight;
-    }
-
-    public void setClientStatus(ClientStatus clientStatus) {
-        this.clientStatus = clientStatus;
-    }
-
     public static enum ClientStatus {
         NONE,
         PENDING,
