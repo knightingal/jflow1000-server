9e84ea81603ee5532327f22b661eb44804afdf13
knightingal <yujianming1985@hotmail.com>
2024-02-27 20:48:41 +0800

    remove unused import

--- src/main/java/org/nanking/knightingal/dao/jpa/Local1000ApkConfigRepo.java
+++ src/main/java/org/nanking/knightingal/dao/jpa/Local1000ApkConfigRepo.java
@@ -1,6 +1,5 @@
 package org.nanking.knightingal.dao.jpa;
 
-import org.nanking.knightingal.bean.AlbumConfig;
 import org.nanking.knightingal.bean.ApkConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
