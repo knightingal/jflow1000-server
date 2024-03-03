d5f0306332aa16b8d6dda949bb3d284bb960f407
knightingal <yujianming1985@hotmail.com>
2024-03-03 23:16:33 +0800

    fit for mustache templates

--- src/main/java/org/nanking/knightingal/controller/WebController.java
+++ src/main/java/org/nanking/knightingal/controller/WebController.java
@@ -1,6 +1,7 @@
 package org.nanking.knightingal.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WebController {
 
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("name", "knightingal");
         return "index";
     }
     
