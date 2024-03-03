a57ddda4adaac3f2027e9e2ecb560e781780e754
knightingal <yujianming1985@hotmail.com>
2024-03-03 23:31:26 +0800

    add qr interface

--- src/main/java/org/nanking/knightingal/controller/WebController.java
+++ src/main/java/org/nanking/knightingal/controller/WebController.java
@@ -1,8 +1,16 @@
 package org.nanking.knightingal.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -14,6 +22,23 @@ public class WebController {
     public String index(Model model) {
         model.addAttribute("name", "knightingal");
         return "index";
+    }
+
+    @GetMapping("/qr")
+    public void qrImage(HttpServletResponse httpServletResponse) throws IOException {
+        httpServletResponse.addHeader("content-type", "image/png");
+        File qrFile = new File("./qrcode.png");
+        InputStream fileIs = new FileInputStream(qrFile);
+        while (true) {
+            byte[] buffer = new byte[1024];
+            int readLen = fileIs.read(buffer);
+            if (readLen <= 0) {
+                break;
+            }
+            httpServletResponse.getOutputStream().write(buffer, 0, readLen);
+
+        }
+        fileIs.close();
     }
     
 }
