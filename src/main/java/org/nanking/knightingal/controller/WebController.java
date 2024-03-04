2f4b63a3d4ae55f9e236ead53982813efcc203e3
knightingal <yujianming1985@hotmail.com>
2024-03-04 22:59:10 +0800

    use mustache to display apkConfigVo

--- src/main/java/org/nanking/knightingal/controller/WebController.java
+++ src/main/java/org/nanking/knightingal/controller/WebController.java
@@ -4,7 +4,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.criteria.Order;
+import jakarta.persistence.criteria.Predicate;
+import org.nanking.knightingal.bean.ApkConfig;
+import org.nanking.knightingal.dao.Local1000ApkConfigDao;
+import org.nanking.knightingal.vo.ApkConfigVO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +27,43 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/web")
 public class WebController {
 
+    private final Local1000ApkConfigDao local1000ApkConfigDao ;
+
+    @Value("${apk.download.url.prefix}")
+    private String apkDownloadUrlPrefix;
+
+
+    public WebController(Local1000ApkConfigDao local1000ApkConfigDao) {
+        this.local1000ApkConfigDao = local1000ApkConfigDao;
+    }
+
     @GetMapping("/index")
     public String index(Model model) {
-        model.addAttribute("name", "knightingal");
+
+        List<ApkConfig> one = local1000ApkConfigDao.findAll((Specification<ApkConfig>) (root, query, builder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            Order versionCode = builder.desc(root.get("versionCode"));
+            return query
+                    .orderBy(versionCode)
+                    .where(predicates.toArray(new Predicate[]{}))
+                    .getRestriction();
+        });
+
+        List<ApkConfigVO> apkConfigVo = one.stream().map(apkConfig -> ApkConfigVO.builder()
+                .apkName(apkConfig.getApkName())
+                .applicationId(apkConfig.getApplicationId())
+                .versionCode(apkConfig.getVersionCode())
+                .versionName(apkConfig.getVersionName())
+                .downloadUrl(apkDownloadUrlPrefix + toApkVersionedName(apkConfig))
+                .build()).toList();
+        model.addAttribute("apkConfigVoList", apkConfigVo);
+
         return "index";
+    }
+
+
+    private String toApkVersionedName(ApkConfig apkConfig) {
+        return apkConfig.getApplicationId() + "_" + apkConfig.getVersionCode() + "_" + apkConfig.getVersionName() + ".apk";
     }
 
     @GetMapping("/qr")
