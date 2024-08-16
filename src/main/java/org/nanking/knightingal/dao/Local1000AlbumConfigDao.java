3609c67256616e33dc6d6b825f93bb29ae0fa1b9
knightingal <yujianming1985@hotmail.com>
2024-08-16 23:11:13 +0800

    add `searchAlbumConfigByName` method

--- src/main/java/org/nanking/knightingal/dao/Local1000AlbumConfigDao.java
+++ src/main/java/org/nanking/knightingal/dao/Local1000AlbumConfigDao.java
@@ -4,9 +4,11 @@ import org.nanking.knightingal.annotation.Repo;
 import org.nanking.knightingal.bean.AlbumConfig;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repo("local1000AlbumConfigRepo")
 public interface Local1000AlbumConfigDao {
+    Optional<AlbumConfig> searchAlbumConfigByName(String name);
 
     List<AlbumConfig> findAll();
 }
--- src/main/java/org/nanking/knightingal/dao/jpa/Local1000AlbumConfigRepo.java
+++ src/main/java/org/nanking/knightingal/dao/jpa/Local1000AlbumConfigRepo.java
@@ -3,6 +3,8 @@ package org.nanking.knightingal.dao.jpa;
 import org.nanking.knightingal.bean.AlbumConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import java.util.Optional;
 
 public interface Local1000AlbumConfigRepo extends JpaRepository<AlbumConfig, Long>, JpaSpecificationExecutor<AlbumConfig> {
+  Optional<AlbumConfig> searchAlbumConfigByName(String name);
 }
