952cf53d8f4646072dd6e5b604524f51ec58ffa7
knightingal <yujianming1985@hotmail.com>
2024-09-02 23:38:09 +0800

    add `parseRGB24`

--- src/test/java/org/nanking/knightingal/QrCodeUtilTest.java
+++ src/test/java/org/nanking/knightingal/QrCodeUtilTest.java
@@ -8,11 +8,17 @@ import java.io.InputStream;
 import org.junit.jupiter.api.Test;
 import org.nanking.knightingal.util.WebpUtil;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.zxing.WriterException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class QrCodeUtilTest {
     @Test
@@ -47,5 +53,33 @@ public class QrCodeUtilTest {
       } catch (Exception e) {
         e.printStackTrace();
       }
+    }
+
+    @Test
+    public void parseRGB24() throws Exception {
+      File rgbFile = new File("/home/knightingal/Videos/Screencasts/Screencast.bin");
+      int width = 1920;
+      int height = 1080;
+      InputStream inputStream = new FileInputStream(rgbFile);
+      List<List<Integer>> colorMatrix = new ArrayList<>();
+      for (int i = 0; i < 200; i++) {
+        List<Integer> colorLine = new ArrayList<>();
+        for (int j = 0; j < width; j++) {
+          byte[] pix = new byte[3];
+          inputStream.read(pix);
+          int r = (int)pix[0] & 0xff;
+          int g = (int)pix[1] & 0xff;
+          int b = (int)pix[2] & 0xff;
+          int color = r << 16 | g << 8 | b;
+          colorLine.add(color);
+        }
+        colorMatrix.add(colorLine);
+      }
+      inputStream.close();
+
+      ObjectMapper objectMapper = new ObjectMapper();
+      objectMapper.writeValue(new File("color.json"), colorMatrix);
+
+
     }
 }
