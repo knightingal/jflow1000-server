2acd7725b3871aab31e79737549cbc1788145fb5
knightingal <yujianming1985@hotmail.com>
2024-09-14 22:49:57 +0800

    make asset store

--- src/main/java/org/nanking/knightingal/controller/InternalApiController.java
+++ src/main/java/org/nanking/knightingal/controller/InternalApiController.java
@@ -1,6 +1,11 @@
 package org.nanking.knightingal.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.http.ResponseEntity;
@@ -8,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Request.Builder;
 import okhttp3.Response;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,28 +22,40 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class InternalApiController {
+  private static final String ASSET_STORE_PATH = "/home/knightingal/Pictures/";
 
   /**
    * call request to fetch asset from static.makerfac.com for scratch
-   * TODO: store and check asset locally
    * @param packageId
    * @return
    * @throws IOException
    */
   @GetMapping("/internalapi/asset/{assetId}")
   public ResponseEntity<byte[]> index(@PathVariable("assetId") String packageId) throws IOException {
+    File assetFile = new File(ASSET_STORE_PATH + packageId);
+    if (assetFile.exists()) {
+      InputStream inputStream = new FileInputStream(assetFile);
+      byte[] allBytes = inputStream.readAllBytes();
+      inputStream.close();
+      String contentType = packageId.endsWith("svg") ? "image/svg+xml":"image/png";
+      return ResponseEntity.ok()
+        .header("content-type", contentType)    
+        .body(allBytes);
+    }
+
+
     String url = "https://static.makerfac.com/static/internalapi/asset/" + packageId;
 
-        Request request = new Request.Builder().url(url).
-                addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36").
-                addHeader("Connection", "keep-alive").
-                addHeader("Accept", "image/webp,image/*,*/*;q=0.8").
-                addHeader("Accept-Encoding", "gzip,deflate,sdch").
-                addHeader("Accept-Language", "zh-CN,zh;q=0.8").
-                addHeader("Pragma","no-cache").
-                addHeader("referer", "https://community.makerfac.com/")
-                addHeader("Cache-Control","no-cache").
-                build();
+    Request request = new Request.Builder().url(url).
+            addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36").
+            addHeader("Connection", "keep-alive").
+            addHeader("Accept", "image/webp,image/*,*/*;q=0.8").
+            addHeader("Accept-Encoding", "gzip,deflate,sdch").
+            addHeader("Accept-Language", "zh-CN,zh;q=0.8").
+            addHeader("Pragma","no-cache").
+            addHeader("referer", "https://community.makerfac.com/").
+            addHeader("Cache-Control","no-cache").
+            build();
     OkHttpClient okHttpClient = new OkHttpClient.Builder()
           .connectTimeout(60, TimeUnit.SECONDS)
           .readTimeout(60, TimeUnit.SECONDS).build();
@@ -45,7 +63,11 @@ public class InternalApiController {
     Response response = okHttpClient.newCall(request).execute();
     byte[] bytes = response.body().bytes();
     response.body().close();
-
+    assetFile.createNewFile();
+    OutputStream outputStream = new FileOutputStream(assetFile);
+    outputStream.write(bytes);
+    outputStream.flush();
+    outputStream.close();
     return ResponseEntity.ok()
       .header("content-type", response.headers().get("content-type"))    
       .body(bytes);
