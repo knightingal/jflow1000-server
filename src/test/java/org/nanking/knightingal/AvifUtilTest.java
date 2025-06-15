28a02d04674b907957287b4452115c126a4484db
knightingal <yujianming1985@hotmail.com>
2025-06-15 10:25:04 +0800

    add unit test

--- src/test/java/org/nanking/knightingal/AvifUtilTest.java
+++ src/test/java/org/nanking/knightingal/AvifUtilTest.java
@@ -1,15 +1,21 @@
 package org.nanking.knightingal;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.io.File;
 import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 import org.nanking.knightingal.util.AvifUtil;
+import org.nanking.knightingal.util.AvifUtil.ImgSize;
 
 public class AvifUtilTest {
   
   @Test
   public void testAvif() throws IOException {
-    AvifUtil.parse(new File("/home/knightingal/Downloads/hato.profile0.10bpc.yuv420.avif"));
+    ImgSize imgSize = AvifUtil.parseImgSize(new File("src/test/hato.profile0.10bpc.yuv420.avif"));
+    assertEquals(imgSize.getHeight(), 2048);
+    assertEquals(imgSize.getWidth(), 3078);
+
   }
 }
