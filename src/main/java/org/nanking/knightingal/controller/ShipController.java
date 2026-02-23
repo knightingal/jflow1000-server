0df25ae5f2d8a3ad7890bb43562d56b15619fa6e
knightingal <yujianming1985@hotmail.com>
2026-02-23 21:31:46 +0800

    import all page

--- src/main/java/org/nanking/knightingal/controller/ShipController.java
+++ src/main/java/org/nanking/knightingal/controller/ShipController.java
@@ -1,5 +1,7 @@
 package org.nanking.knightingal.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.nanking.knightingal.dao.ShipDao;
 import org.nanking.knightingal.ship.Ship;
 import org.nanking.knightingal.util.NaviPageParse;
@@ -9,9 +11,13 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.FilenameFilter;
+
 @RequestMapping("/ship")
 @RestController
 public class ShipController {
+    private static final Logger LOG = LogManager.getLogger(DevController.class);
 
     public ShipController(ShipDao shipDao) {
         this.shipDao = shipDao;
@@ -22,8 +28,25 @@ public class ShipController {
     @GetMapping("/import")
     public ResponseEntity<?> importShip() {
         try {
-            Ship ship = NaviPageParse.parsePage("/home/knightingal/Documents/Battleship Photo Index BB-7 USS ILLINOIS.htm");
-            ship = shipDao.saveAndFlush(ship);
+            File file = new File("/home/knightingal/Documents/");
+            String[] htmFileArray = file.list(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.endsWith(".htm");
+                }
+            });
+
+            for (String htm: htmFileArray) {
+                try {
+                    Ship ship = NaviPageParse.parsePage("/home/knightingal/Documents/" + htm);
+                    ship = shipDao.saveAndFlush(ship);
+                } catch (Exception e) {
+                    LOG.error("cannot parse page:{}, ", htm, e);
+                    throw e;
+                }
+            }
+
+
             return ResponseEntity.ok().build();
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
